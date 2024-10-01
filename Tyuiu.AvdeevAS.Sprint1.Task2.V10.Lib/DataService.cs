432613ce0e68537c8c342acd232f1337ead831b3
Tyuiu.AvdeevAS <Tyuiu.AvdeevAS@outlook.com>
2024-10-01 20:40:18 +0500

    Task2

--- Tyuiu.AvdeevAS.Sprint1.Task2.V10.Lib/DataService.cs
+++ Tyuiu.AvdeevAS.Sprint1.Task2.V10.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.AvdeevAS.Sprint1.Task2.V10.Lib
     {
         public double ConvertMetreToInchs(int value)
         {
-            return Math.Round((value * 39.3701), 3);
+            return Math.Round((value * 39.3701), 2);
         }
     }
 }
